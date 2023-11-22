6089b1dc198e520cb438fbf8fafef2ce81471d79
dcatabian <catabiandb@gmail.com>
2023-11-22 10:11:45 -0700

    peerjs demo

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,91 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <p id="host-id">Your Host ID: </p>
+  <input type="text" id="join-id">
+  <button id="join-enter">JOIN</button>
+  <button id="leave-enter">DISCONNECT</button>
+  <p id="connection-status">Connection status: disconnected</p>
+  <input type="text" id="chat-input">
+  <button id="chat-enter">SEND MESSAGE</button>
+  <div id="chat-box"></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { Peer } from "peerjs";
+import { onMounted } from "vue";
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  setup() {
+    onMounted(()=>{
+      const peer = new Peer();
+
+      //the Elements I made for the demo
+      const host_id = document.getElementById("host-id");
+      const join_id = document.getElementById('join-id')
+      const join_btn = document.getElementById("join-enter");
+      const leave_btn = document.getElementById("leave-enter");
+      const conn_stat = document.getElementById("connection-status");
+      const chat_input = document.getElementById("chat-input");
+      const chat_send = document.getElementById("chat-enter");
+      const chat_box = document.getElementById("chat-box");
+      
+      function connectionEstablished(dataConnection) {
+        dataConnection.on('open', ()=>{
+          conn_stat.innerHTML = `Connection status: connected`;
+          //send info to connected peers
+          chat_send.addEventListener("click", ()=>{
+            const input = chat_input.value
+            chat_box.innerHTML = chat_box.innerHTML.concat("", `<p>${peer.id}: ${input}</p>`);
+            if (input != "") {
+              dataConnection.send(input);
+              chat_input.value = "";
+            }
+          });
+          //get info from connected peers
+          dataConnection.on("data", (data)=>{
+            chat_box.innerHTML = chat_box.innerHTML.concat("", `<p>${dataConnection.peer}: ${data}</p>`);
+          })
+        })
+        dataConnection.on('close', ()=>{
+          conn_stat.innerHTML = `Connection status: disconnected`;
+        })
+        dataConnection.on('error', (e)=>{
+          conn_stat.innerHTML = `Connection status: error ${e}`;
+        })
+      }
+
+      //Peer event listeners
+      peer.on('open', ()=>{
+        host_id.innerHTML = `Your Host ID: ${peer.id}`;
+      })
+
+      peer.on('connection', (dataConnection)=>{
+        connectionEstablished(dataConnection);
+      });
+
+      join_btn.addEventListener("click", ()=>{
+        const input = join_id.value;
+        if(input != "") {
+          const conn = peer.connect(input);
+          connectionEstablished(conn);
+        }
+      })
+
+      leave_btn.addEventListener("click", ()=>{
+        peer.disconnect();
+      })
+
+      document.addEventListener("beforeunload", ()=>{
+        peer.destroy();
+      })
+    })
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+#chat-box {
+  min-height: 2rem;
+  border: 3px solid black;
 }
 </style>
