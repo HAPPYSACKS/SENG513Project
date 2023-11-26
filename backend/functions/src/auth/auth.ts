b3447f126ba1cdbb8775ffe70832aa7181bc6153
HAPPYSACKS <74272531+HAPPYSACKS@users.noreply.github.com>
2023-11-26 05:20:27 -0700

    Removed ESLint, added some functions

--- backend/functions/src/auth/auth.ts
+++ backend/functions/src/auth/auth.ts
@@ -1,25 +1,25 @@
 import * as admin from 'firebase-admin';
-import { Request } from 'express';
+// import { Request } from 'express';
 
 admin.initializeApp();
 
-async function verifyToken(req: Request): Promise<admin.auth.DecodedIdToken> {
-    const authHeader = req.headers.authorization;
+// async function verifyToken(req: Request): Promise<admin.auth.DecodedIdToken> {
+//     const authHeader = req.headers.authorization;
 
-    if (!authHeader) {
-        throw new Error('No token provided');
-    }
+//     if (!authHeader) {
+//         throw new Error('No token provided');
+//     }
 
-    const token = authHeader.split('Bearer ')[1];
-    if (!token) {
-        throw new Error('Bearer token not found in the authorization header');
-    }
+//     const token = authHeader.split('Bearer ')[1];
+//     if (!token) {
+//         throw new Error('Bearer token not found in the authorization header');
+//     }
 
-    try {
-        const decodedToken = await admin.auth().verifyIdToken(token);
-        return decodedToken;
-    } catch (error) {
-        // You can handle specific error types if necessary
-        throw new Error('Invalid token');
-    }
-}
+//     try {
+//         const decodedToken = await admin.auth().verifyIdToken(token);
+//         return decodedToken;
+//     } catch (error) {
+//         // You can handle specific error types if necessary
+//         throw new Error('Invalid token');
+//     }
+// }
