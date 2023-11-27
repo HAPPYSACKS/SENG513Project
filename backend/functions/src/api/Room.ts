f7c244af01c591b2a65cc7e51a1fd2b69ecc83d6
HAPPYSACKS <74272531+HAPPYSACKS@users.noreply.github.com>
2023-11-26 23:22:48 -0700

    Added a room deletion, modified comments

--- backend/functions/src/api/Room.ts
+++ backend/functions/src/api/Room.ts
@@ -51,19 +51,83 @@ exports.addRoom = functions.https.onRequest(
       const newRoomData = {
         CreationTimestamp: new Date().toISOString(), // Set current time as creation timestamp
         LastActivityTimestamp: new Date().toISOString(), // Set current time as last activity timestamp
-        Owner: ownerID, // This could be dynamic based on the authenticated user
+        Owner: ownerID,
         Participants: {},
         Status: "active",
         Widgets: [],
       };
 
-      // Add new room to the 'Room' node
+      // Adds a new room to the 'Room' node
       const roomRef = admin.database().ref("Room");
       const newRoomRef = await roomRef.push(newRoomData);
 
       res.status(200).send({ newRoomId: newRoomRef.key });
     } catch (error) {
       console.error("Error adding new room:", error);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
+
+exports.getAllRooms = functions.https.onRequest(
+  async (req: Request, res: Response) => {
+    // Assumes the request method to be GET
+    if (req.method !== "GET") {
+      res.status(405).send("Method Not Allowed");
+      return;
+    }
+
+    try {
+      const roomsRef = admin.database().ref("Room");
+      const snapshot = await roomsRef.once("value");
+      const roomsData = snapshot.val();
+
+      res.status(200).json(roomsData);
+    } catch (error) {
+      console.error("Error listing rooms:", error);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
+
+exports.deleteRoom = functions.https.onRequest(
+  async (req: Request, res: Response) => {
+    // Assumes the request method to be DELETE
+    if (req.method !== "DELETE") {
+      res.status(405).send("Method Not Allowed");
+      return;
+    }
+
+    const roomID = req.query.roomID;
+    if (!roomID) {
+      res.status(400).send("Room ID is required");
+      return;
+    }
+
+    try {
+      const decodedToken = await verifyToken(req);
+      const userId = decodedToken.uid;
+
+      const roomRef = admin.database().ref(`Room/${roomID}`);
+      const snapshot = await roomRef.once("value");
+      if (!snapshot.exists()) {
+        res.status(404).send("Room not found");
+        return;
+      }
+
+      const roomData = snapshot.val();
+      const isOwner = roomData.Owner === userId;
+
+      // Maybe implement an `admin` check, so that maybe admins can clean up rooms?
+      if (!isOwner) {
+        res.status(403).send("Not authorized to delete this room");
+        return;
+      }
+
+      await roomRef.remove();
+      res.status(200).send(`Room ${roomID} deleted successfully`);
+    } catch (error) {
+      console.error("Error deleting room:", error);
       res.status(500).send("Internal Server Error");
     }
   }
