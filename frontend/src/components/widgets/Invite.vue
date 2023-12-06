b8d0c237f52172932cad4117fb06065453b78ac9
Carlos Sujanto <carlos.sujanto@ucalgary.ca>
2023-12-05 22:38:06 -0700

    added copy function to link

--- frontend/src/components/widgets/Invite.vue
+++ frontend/src/components/widgets/Invite.vue
@@ -1,20 +1,47 @@
 <template>
-    <h3>Room ID:</h3>
-    <h3>{{ this.data.id }}</h3>
-    <p>Other users should copy-paste<br>this into the 'Join' field</p>
+    <div class="container">
+        <p style="cursor: default;">Room ID:</p>
+        <span v-show="copied">copied!</span>
+        <span v-show="!copied">{{ this.data.id }}</span>
+        <i @click="copyLink" style="cursor: pointer;" class="fa fa-clone" aria-hidden="true"></i>
+        <!-- <p>Other users should copy-paste<br>this into the 'Join' field</p> -->
+    </div>
 </template>
 
 <script>
 export default {
+    data(){
+        return{
+            copied: false
+        }
+    },
     name: 'InviteWidget',
     props: {
         data: Object
     },
-    emits: ['update', 'message']
+    emits: ['update', 'message'],
+    methods: {
+        copyLink(){
+            this.copied = !this.copied
+            navigator.clipboard.writeText(this.data.id);
+            setTimeout(() => {
+                this.copied = !this.copied;
+            }, 2000);
+        }
+    }
 }
 </script>
 <style scoped>
 * {
     text-align: center;
+    margin:0;
+}
+
+.container{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 10px;
+    padding: 10px 10px 0 10px;
 }
 </style>
